8941be4d89f4edd0ce83a3c35260278825edf909
Tyler Cox <ktylerc92@gmail.com>
2017-10-29 04:28:24 -0400

    Add maxplayers, tickrate 128, changed default map

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-./srcds_run -game csgo -console -usercon +ip 0.0.0.0 -port 27020 +map de_dust2 +game_type 0 +game_mode 1 -tickrate 84 -autoupdate
+./srcds_run -game csgo -console -usercon +ip 0.0.0.0 -port 27015 +map de_train +game_type 0 +game_mode 1 -tickrate 128 -maxplayers_override 9 -autoupdate
